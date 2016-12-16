6d2d885e475fd26e543b465b70c3476f0fb0e7e1
shareski <shareski@gmail.com>
2016-12-15 20:12:17 -0600

    Added 1 tweets

--- data/js/tweets/2016_12.js
+++ data/js/tweets/2016_12.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2016_12 = 
 [
 	{
+		"created_at": "Fri Dec 16 02:06:58 +0000 2016",
+		"id": 809580547492048900,
+		"id_str": "809580547492048896",
+		"text": "@julie_wenger @KEVAplanks @garisonr @MrLeBrun @dentonisd @TheWeirdTeacher @stemboss That's gold.",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "julie_wenger",
+					"name": "Julie W",
+					"id": 990466885,
+					"id_str": "990466885",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "KEVAplanks",
+					"name": "KEVA Planks",
+					"id": 2941102612,
+					"id_str": "2941102612",
+					"indices": [
+						14,
+						25
+					]
+				},
+				{
+					"screen_name": "garisonr",
+					"name": "Ross Garison",
+					"id": 733400917,
+					"id_str": "733400917",
+					"indices": [
+						26,
+						35
+					]
+				},
+				{
+					"screen_name": "MrLeBrun",
+					"name": "Ross LeBrun",
+					"id": 422572952,
+					"id_str": "422572952",
+					"indices": [
+						36,
+						45
+					]
+				},
+				{
+					"screen_name": "dentonisd",
+					"name": "Denton ISD",
+					"id": 36472219,
+					"id_str": "36472219",
+					"indices": [
+						46,
+						56
+					]
+				},
+				{
+					"screen_name": "TheWeirdTeacher",
+					"name": "Doug Robertson",
+					"id": 492501665,
+					"id_str": "492501665",
+					"indices": [
+						57,
+						73
+					]
+				},
+				{
+					"screen_name": "stemboss",
+					"name": "cindy moss",
+					"id": 724378501,
+					"id_str": "724378501",
+					"indices": [
+						74,
+						83
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 809551943433678800,
+		"in_reply_to_status_id_str": "809551943433678848",
+		"in_reply_to_user_id": 990466885,
+		"in_reply_to_user_id_str": "990466885",
+		"in_reply_to_screen_name": "julie_wenger",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 31546,
+			"friends_count": 5753,
+			"listed_count": 1399,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 589,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 107882,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Dec 15 23:58:03 +0000 2016",
 		"id": 809548102994686000,
 		"id_str": "809548102994685952",
