12127a7e9f51bf6c700281c4aee3c94b0c072f22
shareski <shareski@gmail.com>
2016-12-29 23:12:23 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 107653,
-	"created_at": "Fri Dec 30 04:12:34 +0000 2016"
+	"tweets": 107654,
+	"created_at": "Fri Dec 30 05:12:22 +0000 2016"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_12.js",
 		"year": 2016,
 		"var_name": "tweets_2016_12",
-		"tweet_count": 451,
+		"tweet_count": 452,
 		"month": 12
 	},
 	{
