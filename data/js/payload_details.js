15b3e5492805f4610d3b4be5ed86defa9d441418
shareski <shareski@gmail.com>
2017-02-07 15:12:19 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 108201,
-	"created_at": "Tue Feb 07 19:12:23 +0000 2017"
+	"tweets": 108202,
+	"created_at": "Tue Feb 07 21:12:18 +0000 2017"
 }
--- data/js/tweets/2017_02.js
+++ data/js/tweets/2017_02.js
@@ -1,6 +1,249 @@
 Grailbird.data.tweets_2017_02 = 
 [
 	{
+		"created_at": "Tue Feb 07 20:30:12 +0000 2017",
+		"id": 829064742895509500,
+		"id_str": "829064742895509504",
+		"text": "Okay, this is something I'm pretty jazzed about. https://t.co/1aoM4DDPDh",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1aoM4DDPDh",
+					"expanded_url": "https://twitter.com/ipadpalooza/status/829060918973603840",
+					"display_url": "twitter.com/ipadpalooza/st…",
+					"indices": [
+						49,
+						72
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 32127,
+			"friends_count": 5854,
+			"listed_count": 1415,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 610,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 108653,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 829060918973603800,
+		"quoted_status_id_str": "829060918973603840",
+		"quoted_status": {
+			"created_at": "Tue Feb 07 20:15:00 +0000 2017",
+			"id": 829060918973603800,
+			"id_str": "829060918973603840",
+			"text": "Featured Speaker Announcement! Our first ever father-daughter team @shareski joining us this summer! #iplza17… https://t.co/4X7S2nqMvg",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "iplza17",
+						"indices": [
+							101,
+							109
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "shareski",
+						"name": "Dean Shareski",
+						"id": 739743,
+						"id_str": "739743",
+						"indices": [
+							67,
+							76
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/4X7S2nqMvg",
+						"expanded_url": "https://twitter.com/i/web/status/829060918973603840",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							111,
+							134
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 475895549,
+				"id_str": "475895549",
+				"name": "iPadpalooza",
+				"screen_name": "iPadpalooza",
+				"location": "Austin, Tx",
+				"description": "Mobile Learning Festival taking place June 5-8, 2017 in Austin, TX. All devices welcome! Check out our site for more details https://t.co/JUcmfravhC",
+				"url": "https://t.co/JUcmfravhC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JUcmfravhC",
+								"expanded_url": "http://ipadpalooza.com",
+								"display_url": "ipadpalooza.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/JUcmfravhC",
+								"expanded_url": "http://ipadpalooza.com",
+								"display_url": "ipadpalooza.com",
+								"indices": [
+									125,
+									148
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 3980,
+				"friends_count": 454,
+				"listed_count": 138,
+				"created_at": "Fri Jan 27 15:13:32 +0000 2012",
+				"favourites_count": 485,
+				"utc_offset": -21600,
+				"time_zone": "Central Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1565,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B9B1C2",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/484722902164451329/_SUt2zqa.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/484722902164451329/_SUt2zqa.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/817743756564762629/SfOTmxH0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/817743756564762629/SfOTmxH0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/475895549/1483800280",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Feb 07 18:31:37 +0000 2017",
 		"id": 829034901668294700,
 		"id_str": "829034901668294656",
