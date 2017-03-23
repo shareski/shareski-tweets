6da82b6f6861a6b4dcbc09b7a6de24c797e60852
shareski <shareski@gmail.com>
2017-03-23 17:12:22 -0600

    Added 2 tweets

--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,513 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Thu Mar 23 22:42:38 +0000 2017",
+		"id": 845043138368458800,
+		"id_str": "845043138368458752",
+		"text": "RT @HallDavidson: Orange is the new Dean. You're not alone, friend. @shareski https://t.co/Ql0vRCf4XH",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HallDavidson",
+					"name": "HallDavidson",
+					"id": 7882162,
+					"id_str": "7882162",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "shareski",
+					"name": "Dean Shareski",
+					"id": 739743,
+					"id_str": "739743",
+					"indices": [
+						68,
+						77
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 845034719158792200,
+					"id_str": "845034719158792192",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+					"url": "https://t.co/Ql0vRCf4XH",
+					"display_url": "pic.twitter.com/Ql0vRCf4XH",
+					"expanded_url": "https://twitter.com/HallDavidson/status/845034731917918208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1273,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 746,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 423,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 845034731917918200,
+					"source_status_id_str": "845034731917918208",
+					"source_user_id": 7882162,
+					"source_user_id_str": "7882162"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 845034719158792200,
+					"id_str": "845034719158792192",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+					"url": "https://t.co/Ql0vRCf4XH",
+					"display_url": "pic.twitter.com/Ql0vRCf4XH",
+					"expanded_url": "https://twitter.com/HallDavidson/status/845034731917918208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1273,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 746,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 423,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 845034731917918200,
+					"source_status_id_str": "845034731917918208",
+					"source_user_id": 7882162,
+					"source_user_id_str": "7882162"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 32985,
+			"friends_count": 5987,
+			"listed_count": 1417,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 628,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 109276,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Mar 23 22:09:14 +0000 2017",
+			"id": 845034731917918200,
+			"id_str": "845034731917918208",
+			"text": "Orange is the new Dean. You're not alone, friend. @shareski https://t.co/Ql0vRCf4XH",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "shareski",
+						"name": "Dean Shareski",
+						"id": 739743,
+						"id_str": "739743",
+						"indices": [
+							50,
+							59
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 845034719158792200,
+						"id_str": "845034719158792192",
+						"indices": [
+							60,
+							83
+						],
+						"media_url": "http://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+						"url": "https://t.co/Ql0vRCf4XH",
+						"display_url": "pic.twitter.com/Ql0vRCf4XH",
+						"expanded_url": "https://twitter.com/HallDavidson/status/845034731917918208/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1273,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 746,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 423,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 845034719158792200,
+						"id_str": "845034719158792192",
+						"indices": [
+							60,
+							83
+						],
+						"media_url": "http://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C7oqmZJU8AA-F3U.jpg",
+						"url": "https://t.co/Ql0vRCf4XH",
+						"display_url": "pic.twitter.com/Ql0vRCf4XH",
+						"expanded_url": "https://twitter.com/HallDavidson/status/845034731917918208/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1273,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 746,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 423,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 7882162,
+				"id_str": "7882162",
+				"name": "HallDavidson",
+				"screen_name": "HallDavidson",
+				"location": "Los Angeles",
+				"description": "40 years in education technology. Taught middle/high/college.Worked building teacher network for Discovery (DEN). Now Global ed.Hopeless ed tech geek since '73.",
+				"url": "http://t.co/xKY0xCclOV",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/xKY0xCclOV",
+								"expanded_url": "http://www.discoveryeducation.com/speakers-bureau/index.cfm",
+								"display_url": "discoveryeducation.com/speakers-burea…",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9979,
+				"friends_count": 737,
+				"listed_count": 430,
+				"created_at": "Wed Aug 01 14:32:50 +0000 2007",
+				"favourites_count": 138,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5337,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/378800000851981908/314c39cb9bca69b2ed41c6d6830bc107_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000851981908/314c39cb9bca69b2ed41c6d6830bc107_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/7882162/1402421527",
+				"profile_link_color": "0000FF",
+				"profile_sidebar_border_color": "87BC44",
+				"profile_sidebar_fill_color": "E0FF92",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Mar 23 22:42:26 +0000 2017",
+		"id": 845043084601671700,
+		"id_str": "845043084601671682",
+		"text": "@thomascmurray Nope.  Two hours south. #yqr",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "yqr",
+					"indices": [
+						39,
+						43
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thomascmurray",
+					"name": "Tom Murray",
+					"id": 50447280,
+					"id_str": "50447280",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 845038973932245000,
+		"in_reply_to_status_id_str": "845038973932244993",
+		"in_reply_to_user_id": 50447280,
+		"in_reply_to_user_id_str": "50447280",
+		"in_reply_to_screen_name": "thomascmurray",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 32985,
+			"friends_count": 5987,
+			"listed_count": 1417,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 628,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 109276,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/736041940035063808/6HpLtt-Q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Mar 23 19:09:28 +0000 2017",
 		"id": 844989492859715600,
 		"id_str": "844989492859715584",
