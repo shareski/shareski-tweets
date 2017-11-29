fa9868e10fadc6da29fd5268e4bb2378020370c7
shareski <shareski@gmail.com>
2017-11-29 17:12:19 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111026,
-	"created_at": "Wed Nov 29 15:12:21 +0000 2017"
+	"tweets": 111027,
+	"created_at": "Wed Nov 29 23:12:18 +0000 2017"
 }
--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Wed Nov 29 23:09:20 +0000 2017",
+		"id": 936009208033550300,
+		"id_str": "936009208033550338",
+		"text": "@BryanPDoyle  https://t.co/CLmqQ8vQrv",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BryanPDoyle",
+					"name": "Bryan Doyle",
+					"id": 24112211,
+					"id_str": "24112211",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 936009196985708500,
+					"id_str": "936009196985708544",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DP1fZ8zU8AAaMWn.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DP1fZ8zU8AAaMWn.jpg",
+					"url": "https://t.co/CLmqQ8vQrv",
+					"display_url": "pic.twitter.com/CLmqQ8vQrv",
+					"expanded_url": "https://twitter.com/shareski/status/936009208033550338/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 180,
+							"h": 120,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 120,
+							"h": 120,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 180,
+							"h": 120,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 180,
+							"h": 120,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 936009196985708500,
+					"id_str": "936009196985708544",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DP1fZ8zU8AAaMWn.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DP1fZ8zU8AAaMWn.jpg",
+					"url": "https://t.co/CLmqQ8vQrv",
+					"display_url": "pic.twitter.com/CLmqQ8vQrv",
+					"expanded_url": "https://twitter.com/shareski/status/936009208033550338/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 180,
+							"h": 120,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 120,
+							"h": 120,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 180,
+							"h": 120,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 180,
+							"h": 120,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							3,
+							2
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DP1fZ8zU8AAaMWn.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 936000928355946500,
+		"in_reply_to_status_id_str": "936000928355946498",
+		"in_reply_to_user_id": 24112211,
+		"in_reply_to_user_id_str": "24112211",
+		"in_reply_to_screen_name": "BryanPDoyle",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36185,
+			"friends_count": 6494,
+			"listed_count": 1414,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 652,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 111452,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Wed Nov 29 14:24:10 +0000 2017",
 		"id": 935877048244904000,
 		"id_str": "935877048244903936",
