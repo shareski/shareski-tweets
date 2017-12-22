15861017182a53f0c4bef37fca18cfbc1fae8cfe
shareski <shareski@gmail.com>
2017-12-22 08:12:20 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111220,
-	"created_at": "Fri Dec 22 09:12:20 +0000 2017"
+	"tweets": 111221,
+	"created_at": "Fri Dec 22 14:12:18 +0000 2017"
 }
--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,331 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Fri Dec 22 13:46:00 +0000 2017",
+		"id": 944202363883044900,
+		"id_str": "944202363883044864",
+		"text": "The #deanie for outstanding @DiscoveryEd support for her school goes to @KelliHolden https://t.co/bgmkQFKHfs",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "deanie",
+					"indices": [
+						4,
+						11
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DiscoveryEd",
+					"name": "Discovery Education",
+					"id": 1665531,
+					"id_str": "1665531",
+					"indices": [
+						28,
+						40
+					]
+				},
+				{
+					"screen_name": "KelliHolden",
+					"name": "Kelli Holden",
+					"id": 299916676,
+					"id_str": "299916676",
+					"indices": [
+						72,
+						84
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/bgmkQFKHfs",
+					"expanded_url": "https://twitter.com/MrsHoldensclass/status/940645484846129153",
+					"display_url": "twitter.com/MrsHoldensclas…",
+					"indices": [
+						85,
+						108
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36342,
+			"friends_count": 6536,
+			"listed_count": 1412,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 652,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 111645,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 940645484846129200,
+		"quoted_status_id_str": "940645484846129153",
+		"quoted_status": {
+			"created_at": "Tue Dec 12 18:12:14 +0000 2017",
+			"id": 940645484846129200,
+			"id_str": "940645484846129153",
+			"text": "We were excited to see Canadian celebrations come up on the screen! #CelebrateWithDE #prescottpsd70 https://t.co/Oilu8vT87I",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "CelebrateWithDE",
+						"indices": [
+							68,
+							84
+						]
+					},
+					{
+						"text": "prescottpsd70",
+						"indices": [
+							85,
+							99
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 940645482367299600,
+						"id_str": "940645482367299584",
+						"indices": [
+							100,
+							123
+						],
+						"media_url": "http://pbs.twimg.com/media/DQ3YFRwUMAALkNA.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQ3YFRwUMAALkNA.jpg",
+						"url": "https://t.co/Oilu8vT87I",
+						"display_url": "pic.twitter.com/Oilu8vT87I",
+						"expanded_url": "https://twitter.com/MrsHoldensclass/status/940645484846129153/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1954,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1145,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 649,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 940645482367299600,
+						"id_str": "940645482367299584",
+						"indices": [
+							100,
+							123
+						],
+						"media_url": "http://pbs.twimg.com/media/DQ3YFRwUMAALkNA.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQ3YFRwUMAALkNA.jpg",
+						"url": "https://t.co/Oilu8vT87I",
+						"display_url": "pic.twitter.com/Oilu8vT87I",
+						"expanded_url": "https://twitter.com/MrsHoldensclass/status/940645484846129153/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1954,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1145,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 649,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1729035458,
+				"id_str": "1729035458",
+				"name": "MrsHoldensClass",
+				"screen_name": "MrsHoldensclass",
+				"location": "",
+				"description": "We are a class in Spruce Grove, AB. We love books and learning!",
+				"url": "https://t.co/boa0cR7SLj",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/boa0cR7SLj",
+								"expanded_url": "http://www.psdblogs.ca/kholden",
+								"display_url": "psdblogs.ca/kholden",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 610,
+				"friends_count": 169,
+				"listed_count": 21,
+				"created_at": "Wed Sep 04 15:04:34 +0000 2013",
+				"favourites_count": 140,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 1894,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/771481920421056513/f-uwCaY9_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/771481920421056513/f-uwCaY9_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Dec 22 08:44:00 +0000 2017",
 		"id": 944126361714573300,
 		"id_str": "944126361714573312",
