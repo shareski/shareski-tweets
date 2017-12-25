df78c83fd25c4eb489b9bc72b172260f1c662442
shareski <shareski@gmail.com>
2017-12-24 19:12:20 -0600

    Added 1 tweets

--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,381 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Mon Dec 25 00:18:28 +0000 2017",
+		"id": 945086303087091700,
+		"id_str": "945086303087091712",
+		"text": "RT @knelsonedu: @shareski yes. Yes. YES. https://t.co/Ds1mgbjs2p",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "knelsonedu",
+					"name": "Karen Nelson",
+					"id": 2377885872,
+					"id_str": "2377885872",
+					"indices": [
+						3,
+						14
+					]
+				},
+				{
+					"screen_name": "shareski",
+					"name": "Dean Shareski",
+					"id": 739743,
+					"id_str": "739743",
+					"indices": [
+						16,
+						25
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 945084036799660000,
+					"id_str": "945084036799660033",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+					"url": "https://t.co/Ds1mgbjs2p",
+					"display_url": "pic.twitter.com/Ds1mgbjs2p",
+					"expanded_url": "https://twitter.com/knelsonedu/status/945084042185203712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 632,
+							"h": 842,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 632,
+							"h": 842,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 945084042185203700,
+					"source_status_id_str": "945084042185203712",
+					"source_user_id": 2377885872,
+					"source_user_id_str": "2377885872"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 945084036799660000,
+					"id_str": "945084036799660033",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+					"url": "https://t.co/Ds1mgbjs2p",
+					"display_url": "pic.twitter.com/Ds1mgbjs2p",
+					"expanded_url": "https://twitter.com/knelsonedu/status/945084042185203712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 632,
+							"h": 842,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 632,
+							"h": 842,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 945084042185203700,
+					"source_status_id_str": "945084042185203712",
+					"source_user_id": 2377885872,
+					"source_user_id_str": "2377885872"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36389,
+			"friends_count": 6542,
+			"listed_count": 1413,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 652,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 111719,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Dec 25 00:09:29 +0000 2017",
+			"id": 945084042185203700,
+			"id_str": "945084042185203712",
+			"text": "@shareski yes. Yes. YES. https://t.co/Ds1mgbjs2p",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "shareski",
+						"name": "Dean Shareski",
+						"id": 739743,
+						"id_str": "739743",
+						"indices": [
+							0,
+							9
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 945084036799660000,
+						"id_str": "945084036799660033",
+						"indices": [
+							25,
+							48
+						],
+						"media_url": "http://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+						"url": "https://t.co/Ds1mgbjs2p",
+						"display_url": "pic.twitter.com/Ds1mgbjs2p",
+						"expanded_url": "https://twitter.com/knelsonedu/status/945084042185203712/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 632,
+								"h": 842,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 632,
+								"h": 842,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 945084036799660000,
+						"id_str": "945084036799660033",
+						"indices": [
+							25,
+							48
+						],
+						"media_url": "http://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DR2c7JhWAAEZjBa.jpg",
+						"url": "https://t.co/Ds1mgbjs2p",
+						"display_url": "pic.twitter.com/Ds1mgbjs2p",
+						"expanded_url": "https://twitter.com/knelsonedu/status/945084042185203712/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 632,
+								"h": 842,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 632,
+								"h": 842,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 944632255309336600,
+			"in_reply_to_status_id_str": "944632255309336576",
+			"in_reply_to_user_id": 739743,
+			"in_reply_to_user_id_str": "739743",
+			"in_reply_to_screen_name": "shareski",
+			"user": {
+				"id": 2377885872,
+				"id_str": "2377885872",
+				"name": "Karen Nelson",
+				"screen_name": "knelsonedu",
+				"location": "",
+				"description": "Wife, mother, teacher, learner. NBCT.\nDoing ordinary things with extraordinary love.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 729,
+				"friends_count": 783,
+				"listed_count": 23,
+				"created_at": "Sat Mar 08 01:06:29 +0000 2014",
+				"favourites_count": 4358,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1483,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFCC4D",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/554384800262258689/P4hnNj4h.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/554384800262258689/P4hnNj4h.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/556544497144721408/9G-ZmmMm_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/556544497144721408/9G-ZmmMm_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2377885872/1421012448",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Dec 24 22:32:18 +0000 2017",
 		"id": 945059585718669300,
 		"id_str": "945059585718669317",
