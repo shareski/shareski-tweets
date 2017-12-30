b5f190117e10bc697a5702cc784b80e903815bcd
shareski <shareski@gmail.com>
2017-12-30 10:12:22 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111404,
-	"created_at": "Sat Dec 30 13:12:23 +0000 2017"
+	"tweets": 111405,
+	"created_at": "Sat Dec 30 16:12:21 +0000 2017"
 }
--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,391 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Sat Dec 30 15:46:00 +0000 2017",
+		"id": 947131665318596600,
+		"id_str": "947131665318596608",
+		"text": "The #deanie for best rendition of my SurprisinglyAwesome workshop goes to ... @TeacherJenYoung https://t.co/zADqGDhr6O",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "deanie",
+					"indices": [
+						4,
+						11
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TeacherJenYoung",
+					"name": "Jennifer Young",
+					"id": 541039653,
+					"id_str": "541039653",
+					"indices": [
+						78,
+						94
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zADqGDhr6O",
+					"expanded_url": "https://twitter.com/TeacherJenYoung/status/864295465839407104",
+					"display_url": "twitter.com/TeacherJenYoun…",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36419,
+			"friends_count": 6550,
+			"listed_count": 1411,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 652,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 111829,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 864295465839407100,
+		"quoted_status_id_str": "864295465839407104",
+		"quoted_status": {
+			"created_at": "Tue May 16 01:44:31 +0000 2017",
+			"id": 864295465839407100,
+			"id_str": "864295465839407104",
+			"text": "@shareski gotta check this #SuprisinglyAwesome video! #rtwgimli #mondayfunday https://t.co/EkwC6EpoTT",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SuprisinglyAwesome",
+						"indices": [
+							27,
+							46
+						]
+					},
+					{
+						"text": "rtwgimli",
+						"indices": [
+							54,
+							63
+						]
+					},
+					{
+						"text": "mondayfunday",
+						"indices": [
+							64,
+							77
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "shareski",
+						"name": "Dean Shareski",
+						"id": 739743,
+						"id_str": "739743",
+						"indices": [
+							0,
+							9
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 864294937076023300,
+						"id_str": "864294937076023296",
+						"indices": [
+							78,
+							101
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/864294937076023296/pu/img/CBdJQg_P6dMj-yVz.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/864294937076023296/pu/img/CBdJQg_P6dMj-yVz.jpg",
+						"url": "https://t.co/EkwC6EpoTT",
+						"display_url": "pic.twitter.com/EkwC6EpoTT",
+						"expanded_url": "https://twitter.com/TeacherJenYoung/status/864295465839407104/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 864294937076023300,
+						"id_str": "864294937076023296",
+						"indices": [
+							78,
+							101
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/864294937076023296/pu/img/CBdJQg_P6dMj-yVz.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/864294937076023296/pu/img/CBdJQg_P6dMj-yVz.jpg",
+						"url": "https://t.co/EkwC6EpoTT",
+						"display_url": "pic.twitter.com/EkwC6EpoTT",
+						"expanded_url": "https://twitter.com/TeacherJenYoung/status/864295465839407104/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 67245,
+							"variants": [
+								{
+									"bitrate": 320000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/864294937076023296/pu/vid/320x180/DSdN1KSJPbh-joiv.mp4"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/864294937076023296/pu/pl/3YPlMYDAEmJW2yOG.m3u8"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/864294937076023296/pu/vid/640x360/oswue60zt7oY2O9i.mp4"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/864294937076023296/pu/vid/1280x720/Wjn-q_hJNgauXb1h.mp4"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": 739743,
+			"in_reply_to_user_id_str": "739743",
+			"in_reply_to_screen_name": "shareski",
+			"user": {
+				"id": 541039653,
+				"id_str": "541039653",
+				"name": "Jennifer Young",
+				"screen_name": "TeacherJenYoung",
+				"location": "Manitoba, Canada",
+				"description": "Teaching where the sunflowers grow! Ah... Manitoba! President of MART. #MIEExpert #AppleTeacher Views are my own.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1336,
+				"friends_count": 1521,
+				"listed_count": 85,
+				"created_at": "Fri Mar 30 16:37:05 +0000 2012",
+				"favourites_count": 3212,
+				"utc_offset": -21600,
+				"time_zone": "Central Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4422,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/922180422758490113/1E7iEEF0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/922180422758490113/1E7iEEF0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/541039653/1371225517",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "74c46bc36f4263ac",
+				"url": "https://api.twitter.com/1.1/geo/id/74c46bc36f4263ac.json",
+				"place_type": "city",
+				"name": "Emerson",
+				"full_name": "Emerson, Manitoba",
+				"country_code": "CA",
+				"country": "Canada",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-97.260226,
+								49.000485
+							],
+							[
+								-97.167136,
+								49.000485
+							],
+							[
+								-97.167136,
+								49.03761
+							],
+							[
+								-97.260226,
+								49.03761
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Dec 30 12:39:00 +0000 2017",
 		"id": 947084604678082600,
 		"id_str": "947084604678082560",
