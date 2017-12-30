1b90c55d2185c69c9fdc1a18252e259e945e402b
shareski <shareski@gmail.com>
2017-12-29 23:12:23 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111401,
-	"created_at": "Sat Dec 30 04:12:20 +0000 2017"
+	"tweets": 111402,
+	"created_at": "Sat Dec 30 05:12:21 +0000 2017"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_12.js",
 		"year": 2017,
 		"var_name": "tweets_2017_12",
-		"tweet_count": 371,
+		"tweet_count": 372,
 		"month": 12
 	},
 	{
