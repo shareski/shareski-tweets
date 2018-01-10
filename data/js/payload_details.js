b5b014d01bb0c5e4e574588df5a57b70f2e8c15d
shareski <shareski@gmail.com>
2018-01-10 11:12:20 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111529,
-	"created_at": "Wed Jan 10 16:12:17 +0000 2018"
+	"tweets": 111530,
+	"created_at": "Wed Jan 10 17:12:19 +0000 2018"
 }
--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Wed Jan 10 16:46:01 +0000 2018",
+		"id": 951133037139578900,
+		"id_str": "951133037139578880",
+		"text": "@wmchamberlain @itechbob @MatthewWeld @Carteea @DrClintFreeman @EducPlus The good news is I may be in Missouri a bi… https://t.co/u6gvsK6rOH",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wmchamberlain",
+					"name": "William Chamberlain",
+					"id": 10314482,
+					"id_str": "10314482",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "itechbob",
+					"name": "Bob Deneau",
+					"id": 14298483,
+					"id_str": "14298483",
+					"indices": [
+						15,
+						24
+					]
+				},
+				{
+					"screen_name": "MatthewWeld",
+					"name": "Matt Weld",
+					"id": 441477298,
+					"id_str": "441477298",
+					"indices": [
+						25,
+						37
+					]
+				},
+				{
+					"screen_name": "Carteea",
+					"name": "Angela Cartee",
+					"id": 11165862,
+					"id_str": "11165862",
+					"indices": [
+						38,
+						46
+					]
+				},
+				{
+					"screen_name": "DrClintFreeman",
+					"name": "Dr. Clint J. Freeman",
+					"id": 450220519,
+					"id_str": "450220519",
+					"indices": [
+						47,
+						62
+					]
+				},
+				{
+					"screen_name": "EducPlus",
+					"name": "EducationPlus",
+					"id": 148064284,
+					"id_str": "148064284",
+					"indices": [
+						63,
+						72
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/u6gvsK6rOH",
+					"expanded_url": "https://twitter.com/i/web/status/951133037139578880",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 951119713945423900,
+		"in_reply_to_status_id_str": "951119713945423872",
+		"in_reply_to_user_id": 10314482,
+		"in_reply_to_user_id_str": "10314482",
+		"in_reply_to_screen_name": "wmchamberlain",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working everyday to bring a little more joy into the world. Building community for Discovery Education. Founder of #napchat FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								129,
+								152
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36495,
+			"friends_count": 6567,
+			"listed_count": 1412,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 651,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 111953,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Jan 10 15:39:33 +0000 2018",
 		"id": 951116310041301000,
 		"id_str": "951116310041300992",
