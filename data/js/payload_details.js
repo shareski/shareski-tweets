404cd609d1d21b40baaab879835bb63ef1c84aa5
shareski <shareski@gmail.com>
2018-01-17 23:12:18 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111592,
-	"created_at": "Thu Jan 18 02:12:18 +0000 2018"
+	"tweets": 111593,
+	"created_at": "Thu Jan 18 05:12:17 +0000 2018"
 }
--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Thu Jan 18 04:59:27 +0000 2018",
+		"id": 953854325779988500,
+		"id_str": "953854325779988480",
+		"text": "@MatthewWeld @itechbob @Carteea @DrClintFreeman @EducPlus If you're at METC, look for an opportunity for a dinner or connecting opportunity.",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MatthewWeld",
+					"name": "Matt Weld",
+					"id": 441477298,
+					"id_str": "441477298",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "itechbob",
+					"name": "Bob Deneau",
+					"id": 14298483,
+					"id_str": "14298483",
+					"indices": [
+						13,
+						22
+					]
+				},
+				{
+					"screen_name": "Carteea",
+					"name": "Angela Cartee",
+					"id": 11165862,
+					"id_str": "11165862",
+					"indices": [
+						23,
+						31
+					]
+				},
+				{
+					"screen_name": "DrClintFreeman",
+					"name": "Dr. Clint J. Freeman",
+					"id": 450220519,
+					"id_str": "450220519",
+					"indices": [
+						32,
+						47
+					]
+				},
+				{
+					"screen_name": "EducPlus",
+					"name": "EducationPlus",
+					"id": 148064284,
+					"id_str": "148064284",
+					"indices": [
+						48,
+						57
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 953846032022024200,
+		"in_reply_to_status_id_str": "953846032022024193",
+		"in_reply_to_user_id": 441477298,
+		"in_reply_to_user_id_str": "441477298",
+		"in_reply_to_screen_name": "MatthewWeld",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world. Building community for @DiscoveryED . Founder of #napchat FYI: https://t.co/GsJVzEwnOh",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEwnOh",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								124,
+								147
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36563,
+			"friends_count": 6569,
+			"listed_count": 1409,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 651,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 112016,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Jan 18 02:05:42 +0000 2018",
 		"id": 953810599825367000,
 		"id_str": "953810599825367040",
