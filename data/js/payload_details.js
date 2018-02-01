a37180c79eb62f877ca7b4be4b3ee3c39a1adc17
shareski <shareski@gmail.com>
2018-02-01 12:12:20 -0600

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111720,
-	"created_at": "Wed Jan 31 22:12:20 +0000 2018"
+	"tweets": 111722,
+	"created_at": "Thu Feb 01 18:12:19 +0000 2018"
 }
---
+++ data/js/tweets/2018_02.js
@@ -0,0 +1,533 @@
+Grailbird.data.tweets_2018_02 = 
+[
+	{
+		"created_at": "Thu Feb 01 17:45:09 +0000 2018",
+		"id": 959120451409711100,
+		"id_str": "959120451409711105",
+		"text": "If you have some perspectives and experiences, please share. https://t.co/f7NJF3KE01",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/f7NJF3KE01",
+					"expanded_url": "https://twitter.com/razamirani/status/959116742394982400",
+					"display_url": "twitter.com/razamirani/sta…",
+					"indices": [
+						61,
+						84
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world. Building community for @DiscoveryED . Founder of #napchat FYI: https://t.co/GsJVzEwnOh",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEwnOh",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								124,
+								147
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36658,
+			"friends_count": 6589,
+			"listed_count": 1408,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 653,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 112145,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "00dc41d1ef797d1a",
+			"url": "https://api.twitter.com/1.1/geo/id/00dc41d1ef797d1a.json",
+			"place_type": "city",
+			"name": "Moose Jaw",
+			"full_name": "Moose Jaw, Saskatchewan",
+			"country_code": "CA",
+			"country": "Canada",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-105.626384,
+							50.35293
+						],
+						[
+							-105.465886,
+							50.35293
+						],
+						[
+							-105.465886,
+							50.43446
+						],
+						[
+							-105.626384,
+							50.43446
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 959116742394982400,
+		"quoted_status_id_str": "959116742394982400",
+		"quoted_status": {
+			"created_at": "Thu Feb 01 17:30:25 +0000 2018",
+			"id": 959116742394982400,
+			"id_str": "959116742394982400",
+			"text": "Hi @gcouros @shareski @ChrisKennedy I am looking for feedback on BYOD.  Can you help me get the word out: https://t.co/TJ4MWpXWrE #byod",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "byod",
+						"indices": [
+							130,
+							135
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "gcouros",
+						"name": "George Couros",
+						"id": 18368680,
+						"id_str": "18368680",
+						"indices": [
+							3,
+							11
+						]
+					},
+					{
+						"screen_name": "shareski",
+						"name": "Dean Shareski",
+						"id": 739743,
+						"id_str": "739743",
+						"indices": [
+							12,
+							21
+						]
+					},
+					{
+						"screen_name": "ChrisKennedy",
+						"name": "Chris Kennedy",
+						"id": 12110432,
+						"id_str": "12110432",
+						"indices": [
+							22,
+							35
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/TJ4MWpXWrE",
+						"expanded_url": "http://bit.ly/2GCYsXj",
+						"display_url": "bit.ly/2GCYsXj",
+						"indices": [
+							106,
+							129
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 147271562,
+				"id_str": "147271562",
+				"name": "Raza Mirani",
+				"screen_name": "razamirani",
+				"location": "Vancouver, British Columbia",
+				"description": "Educator + Tech Enthusiast + Wannabe Drummer + Dad + Husband + Son + Suffering Canucks Fan + Child of the 80s + Community Volunteer = Me",
+				"url": "https://t.co/PYDTxfDLPe",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/PYDTxfDLPe",
+								"expanded_url": "http://rmirani.wordpress.com",
+								"display_url": "rmirani.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1587,
+				"friends_count": 2808,
+				"listed_count": 97,
+				"created_at": "Sun May 23 17:37:42 +0000 2010",
+				"favourites_count": 525,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 8919,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "352726",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/465676528999620609/HX9zHh5B_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/465676528999620609/HX9zHh5B_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/147271562/1487742242",
+				"profile_link_color": "D02B55",
+				"profile_sidebar_border_color": "829D5E",
+				"profile_sidebar_fill_color": "99CC33",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Feb 01 17:44:01 +0000 2018",
+		"id": 959120164406091800,
+		"id_str": "959120164406091776",
+		"text": "@mcguirp @DiscoveryEd @daveski61 @OttCatholicSB @OCDSB @OCSB_LT @TCDSB @TDOttawa @DufferinPeel @PeelSchools… https://t.co/ELGnmUmbkL",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mcguirp",
+					"name": "Paul McGuire",
+					"id": 80822931,
+					"id_str": "80822931",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "DiscoveryEd",
+					"name": "Discovery Education",
+					"id": 1665531,
+					"id_str": "1665531",
+					"indices": [
+						9,
+						21
+					]
+				},
+				{
+					"screen_name": "daveski61",
+					"name": "David Tchozewski",
+					"id": 25282643,
+					"id_str": "25282643",
+					"indices": [
+						22,
+						32
+					]
+				},
+				{
+					"screen_name": "OttCatholicSB",
+					"name": "Ottawa Catholic SB",
+					"id": 77040239,
+					"id_str": "77040239",
+					"indices": [
+						33,
+						47
+					]
+				},
+				{
+					"screen_name": "OCDSB",
+					"name": "OCDSB",
+					"id": 209596323,
+					"id_str": "209596323",
+					"indices": [
+						48,
+						54
+					]
+				},
+				{
+					"screen_name": "OCSB_LT",
+					"name": "OCSB LT Department",
+					"id": 712350413102698500,
+					"id_str": "712350413102698497",
+					"indices": [
+						55,
+						63
+					]
+				},
+				{
+					"screen_name": "TCDSB",
+					"name": "Toronto Catholic DSB",
+					"id": 388424709,
+					"id_str": "388424709",
+					"indices": [
+						64,
+						70
+					]
+				},
+				{
+					"screen_name": "TDOttawa",
+					"name": "Tom D'Amico",
+					"id": 23326769,
+					"id_str": "23326769",
+					"indices": [
+						71,
+						80
+					]
+				},
+				{
+					"screen_name": "DufferinPeel",
+					"name": "Dufferin-Peel CDSB",
+					"id": 114818144,
+					"id_str": "114818144",
+					"indices": [
+						81,
+						94
+					]
+				},
+				{
+					"screen_name": "PeelSchools",
+					"name": "Peel District School Board",
+					"id": 23796987,
+					"id_str": "23796987",
+					"indices": [
+						95,
+						107
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ELGnmUmbkL",
+					"expanded_url": "https://twitter.com/i/web/status/959120164406091776",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 959105434799009800,
+		"in_reply_to_status_id_str": "959105434799009792",
+		"in_reply_to_user_id": 80822931,
+		"in_reply_to_user_id_str": "80822931",
+		"in_reply_to_screen_name": "mcguirp",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world. Building community for @DiscoveryED . Founder of #napchat FYI: https://t.co/GsJVzEwnOh",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEwnOh",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								124,
+								147
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36658,
+			"friends_count": 6589,
+			"listed_count": 1408,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 653,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 112145,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "00dc41d1ef797d1a",
+			"url": "https://api.twitter.com/1.1/geo/id/00dc41d1ef797d1a.json",
+			"place_type": "city",
+			"name": "Moose Jaw",
+			"full_name": "Moose Jaw, Saskatchewan",
+			"country_code": "CA",
+			"country": "Canada",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-105.626384,
+							50.35293
+						],
+						[
+							-105.465886,
+							50.35293
+						],
+						[
+							-105.465886,
+							50.43446
+						],
+						[
+							-105.626384,
+							50.43446
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	}
+]
