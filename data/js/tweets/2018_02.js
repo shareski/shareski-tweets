bcfdf761fc4774e6630f8b9783d5ae8b0b74399e
shareski <shareski@gmail.com>
2018-02-20 13:12:20 -0600

    Added 1 tweets

--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Tue Feb 20 18:32:45 +0000 2018",
+		"id": 966017800320897000,
+		"id_str": "966017800320897024",
+		"text": "#truth https://t.co/sXDtvbygSP",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "truth",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sXDtvbygSP",
+					"expanded_url": "https://twitter.com/mikehaire/status/965780415851110400",
+					"display_url": "twitter.com/mikehaire/stat…",
+					"indices": [
+						7,
+						30
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world. Building community for @DiscoveryED . Founder of #napchat FYI: https://t.co/GsJVzEwnOh",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEwnOh",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								124,
+								147
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36845,
+			"friends_count": 6615,
+			"listed_count": 1411,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 652,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 112394,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "01282a8563b05f28",
+			"url": "https://api.twitter.com/1.1/geo/id/01282a8563b05f28.json",
+			"place_type": "city",
+			"name": "Rancho Mirage",
+			"full_name": "Rancho Mirage, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-116.453591,
+							33.7322
+						],
+						[
+							-116.3881016,
+							33.7322
+						],
+						[
+							-116.3881016,
+							33.826269
+						],
+						[
+							-116.453591,
+							33.826269
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 965780415851110400,
+		"quoted_status_id_str": "965780415851110400",
+		"quoted_status": {
+			"created_at": "Tue Feb 20 02:49:29 +0000 2018",
+			"id": 965780415851110400,
+			"id_str": "965780415851110400",
+			"text": "Building capital with kids through small daily positive interactions comes in handy when the time comes for difficult conversations",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 25073272,
+				"id_str": "25073272",
+				"name": "Mike Haire",
+				"screen_name": "MiKEHAiRE",
+				"location": "Abbotsford, BC",
+				"description": "Middle School Vice Principal • Passionate about Learning, Creativity & Kindness • Don't Accept Mediocrity!",
+				"url": "https://t.co/7PSIlYbY8c",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7PSIlYbY8c",
+								"expanded_url": "http://teacheroftheyear.ca",
+								"display_url": "teacheroftheyear.ca",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 616,
+				"friends_count": 578,
+				"listed_count": 33,
+				"created_at": "Wed Mar 18 13:42:20 +0000 2009",
+				"favourites_count": 1762,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4073,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1F1209",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/298457498/273-000000_brick-walls.gif",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/298457498/273-000000_brick-walls.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/749331407831347201/z-zYxgru_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/749331407831347201/z-zYxgru_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/25073272/1467489702",
+				"profile_link_color": "0094DE",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFA319",
+				"profile_text_color": "FFFFFF",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 12,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Tue Feb 20 05:43:05 +0000 2018",
 		"id": 965824105999052800,
 		"id_str": "965824105999052802",
