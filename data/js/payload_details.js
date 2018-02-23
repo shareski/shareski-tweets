f5fa573e5432aa80adaa2d3bba99ee55fa9aa42f
shareski <shareski@gmail.com>
2018-02-22 22:12:21 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 111992,
-	"created_at": "Fri Feb 23 02:12:20 +0000 2018"
+	"tweets": 111993,
+	"created_at": "Fri Feb 23 04:12:20 +0000 2018"
 }
--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Fri Feb 23 03:18:05 +0000 2018",
+		"id": 966874777385238500,
+		"id_str": "966874777385238530",
+		"text": "@paddlingsupt @DSchuler1970 Tell me about David’s approach/style I know @DSchuler1970 does good work. Curious to hear your view.",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "paddlingsupt",
+					"name": "Chris Gaines",
+					"id": 731162456,
+					"id_str": "731162456",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "DSchuler1970",
+					"name": "David Schuler",
+					"id": 2163388556,
+					"id_str": "2163388556",
+					"indices": [
+						14,
+						27
+					]
+				},
+				{
+					"screen_name": "DSchuler1970",
+					"name": "David Schuler",
+					"id": 2163388556,
+					"id_str": "2163388556",
+					"indices": [
+						72,
+						85
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 966848493573820400,
+		"in_reply_to_status_id_str": "966848493573820416",
+		"in_reply_to_user_id": 731162456,
+		"in_reply_to_user_id_str": "731162456",
+		"in_reply_to_screen_name": "paddlingsupt",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world. Building community for @DiscoveryED . Founder of #napchat FYI: https://t.co/GsJVzEwnOh",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEwnOh",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								124,
+								147
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 36869,
+			"friends_count": 6618,
+			"listed_count": 1410,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 652,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 112411,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "59b1b9cae0994377",
+			"url": "https://api.twitter.com/1.1/geo/id/59b1b9cae0994377.json",
+			"place_type": "city",
+			"name": "Sooke",
+			"full_name": "Sooke, British Columbia",
+			"country_code": "CA",
+			"country": "Canada",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-123.784831,
+							48.336895
+						],
+						[
+							-123.597297,
+							48.336895
+						],
+						[
+							-123.597297,
+							48.474038
+						],
+						[
+							-123.784831,
+							48.474038
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Feb 23 01:30:00 +0000 2018",
 		"id": 966847578816983000,
 		"id_str": "966847578816983040",
