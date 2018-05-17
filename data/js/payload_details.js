6fd0414f5880e911922ce1215ade94f78185b181
shareski <shareski@gmail.com>
2018-05-17 11:12:20 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 112710,
-	"created_at": "Thu May 17 16:12:19 +0000 2018"
+	"tweets": 112711,
+	"created_at": "Thu May 17 17:12:19 +0000 2018"
 }
--- data/js/tweets/2018_05.js
+++ data/js/tweets/2018_05.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2018_05 = 
 [
 	{
+		"created_at": "Thu May 17 17:06:56 +0000 2018",
+		"id": 997161556348690400,
+		"id_str": "997161556348690432",
+		"text": "Just know @bbray27 does good work. Always a pleasure to speak with her. https://t.co/Y1TWob2fSY",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bbray27",
+					"name": "Barbara Bray",
+					"id": 6536022,
+					"id_str": "6536022",
+					"indices": [
+						10,
+						18
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Y1TWob2fSY",
+					"expanded_url": "https://twitter.com/bbray27/status/997151583501627392",
+					"display_url": "twitter.com/bbray27/status…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world. Building community for @DiscoveryED . Founder of #napchat FYI: https://t.co/GsJVzEwnOh",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEwnOh",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								124,
+								147
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 37605,
+			"friends_count": 6756,
+			"listed_count": 1404,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 650,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 113057,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/5436963/twitterback.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 997151583501627400,
+		"quoted_status_id_str": "997151583501627392",
+		"quoted_status": {
+			"created_at": "Thu May 17 16:27:18 +0000 2018",
+			"id": 997151583501627400,
+			"id_str": "997151583501627392",
+			"text": "How do we ensure our students experience joy and wonder every day? Check out Episode #42 https://t.co/nRvzLc71nq …… https://t.co/mMxQAB9KkX",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/nRvzLc71nq",
+						"expanded_url": "http://bit.ly/episode42-shareski",
+						"display_url": "bit.ly/episode42-shar…",
+						"indices": [
+							89,
+							112
+						]
+					},
+					{
+						"url": "https://t.co/mMxQAB9KkX",
+						"expanded_url": "https://twitter.com/i/web/status/997151583501627392",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							116,
+							139
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 6536022,
+				"id_str": "6536022",
+				"name": "Barbara Bray",
+				"screen_name": "bbray27",
+				"location": "SF Bay Area",
+				"description": "Storyteller, dreamer, podcaster, & changemaker using #ikigai #UDL #designthinking #PBL #SDGs #play & #agency to find your passion & purpose to #DefineYourWhy",
+				"url": "https://t.co/MbuWqh8n1H",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/MbuWqh8n1H",
+								"expanded_url": "http://barbarabray.net",
+								"display_url": "barbarabray.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10843,
+				"friends_count": 9422,
+				"listed_count": 667,
+				"created_at": "Sun Jun 03 05:43:52 +0000 2007",
+				"favourites_count": 31657,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 23123,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "09CAF0",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000156170516/xmZY0ST_.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000156170516/xmZY0ST_.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/814751779694907397/U6RZvP1B_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/814751779694907397/U6RZvP1B_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/6536022/1483384690",
+				"profile_link_color": "FF691F",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "B0E5FC",
+				"profile_text_color": "3D8C0D",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu May 17 15:47:06 +0000 2018",
 		"id": 997141467633102800,
 		"id_str": "997141467633102848",
