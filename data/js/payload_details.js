380631770c42b9e44bf130fd4766a4ab2eaed9cf
shareski <shareski@gmail.com>
2018-05-28 12:12:23 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 112783,
-	"created_at": "Mon May 28 05:12:18 +0000 2018"
+	"tweets": 112784,
+	"created_at": "Mon May 28 18:12:21 +0000 2018"
 }
--- data/js/tweets/2018_05.js
+++ data/js/tweets/2018_05.js
@@ -1,6 +1,458 @@
 Grailbird.data.tweets_2018_05 = 
 [
 	{
+		"created_at": "Mon May 28 17:47:19 +0000 2018",
+		"id": 1001157987124342800,
+		"id_str": "1001157987124342785",
+		"text": "Big fan of what #sd37 is doing here. https://t.co/SDQugNfSfx",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sd37",
+					"indices": [
+						16,
+						21
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SDQugNfSfx",
+					"expanded_url": "https://twitter.com/bmooreintheloop/status/1001128196157292544",
+					"display_url": "twitter.com/bmooreintheloo…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world.  Currently ranked #12. Building community for @DiscoveryED . FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								127,
+								150
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 37540,
+			"friends_count": 6766,
+			"listed_count": 1406,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 650,
+			"utc_offset": -21600,
+			"time_zone": "Saskatchewan",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 113128,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1001128196157292500,
+		"quoted_status_id_str": "1001128196157292544",
+		"quoted_status": {
+			"created_at": "Mon May 28 15:48:56 +0000 2018",
+			"id": 1001128196157292500,
+			"id_str": "1001128196157292544",
+			"text": "Morning at @deltafarmroots!!! https://t.co/ckQV1Pl8Hp",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "deltafarmroots",
+						"name": "Farm Roots",
+						"id": 4731216253,
+						"id_str": "4731216253",
+						"indices": [
+							11,
+							26
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1001128160291799000,
+						"id_str": "1001128160291799040",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/DeS4wfDVQAAFkbE.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DeS4wfDVQAAFkbE.jpg",
+						"url": "https://t.co/ckQV1Pl8Hp",
+						"display_url": "pic.twitter.com/ckQV1Pl8Hp",
+						"expanded_url": "https://twitter.com/bmooreintheloop/status/1001128196157292544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1001128160291799000,
+						"id_str": "1001128160291799040",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/DeS4wfDVQAAFkbE.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DeS4wfDVQAAFkbE.jpg",
+						"url": "https://t.co/ckQV1Pl8Hp",
+						"display_url": "pic.twitter.com/ckQV1Pl8Hp",
+						"expanded_url": "https://twitter.com/bmooreintheloop/status/1001128196157292544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1001128160291778600,
+						"id_str": "1001128160291778561",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/DeS4wfDU8AEvdm1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DeS4wfDU8AEvdm1.jpg",
+						"url": "https://t.co/ckQV1Pl8Hp",
+						"display_url": "pic.twitter.com/ckQV1Pl8Hp",
+						"expanded_url": "https://twitter.com/bmooreintheloop/status/1001128196157292544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1001128160295972900,
+						"id_str": "1001128160295972869",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/DeS4wfEU8AUeFn8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DeS4wfEU8AUeFn8.jpg",
+						"url": "https://t.co/ckQV1Pl8Hp",
+						"display_url": "pic.twitter.com/ckQV1Pl8Hp",
+						"expanded_url": "https://twitter.com/bmooreintheloop/status/1001128196157292544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1001128160283451400,
+						"id_str": "1001128160283451394",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/DeS4wfBV4AI3srF.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DeS4wfBV4AI3srF.jpg",
+						"url": "https://t.co/ckQV1Pl8Hp",
+						"display_url": "pic.twitter.com/ckQV1Pl8Hp",
+						"expanded_url": "https://twitter.com/bmooreintheloop/status/1001128196157292544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 169306882,
+				"id_str": "169306882",
+				"name": "Brooke Moore",
+				"screen_name": "bmooreintheloop",
+				"location": "Delta, British Columbia",
+				"description": "Learner! Educator. Principal of Innovation and Inquiry in Delta. Writer. Mother. Editor-in-Chief, https://t.co/KfrMpQIybs",
+				"url": "https://t.co/MHEYaZcj5c",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/MHEYaZcj5c",
+								"expanded_url": "http://www.deltalearns.ca/farmroots",
+								"display_url": "deltalearns.ca/farmroots",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/KfrMpQIybs",
+								"expanded_url": "http://www.teljournal.ca",
+								"display_url": "teljournal.ca",
+								"indices": [
+									98,
+									121
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 2075,
+				"friends_count": 1362,
+				"listed_count": 0,
+				"created_at": "Thu Jul 22 01:22:41 +0000 2010",
+				"favourites_count": 2694,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3184,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/981421824553766913/Pxcotaae_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/981421824553766913/Pxcotaae_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/169306882/1522825403",
+				"profile_link_color": "1F98C7",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "5f49a5792faa1e89",
+				"url": "https://api.twitter.com/1.1/geo/id/5f49a5792faa1e89.json",
+				"place_type": "city",
+				"name": "Delta",
+				"full_name": "Delta, British Columbia",
+				"country_code": "CA",
+				"country": "Canada",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-123.3520628,
+								49.001938
+							],
+							[
+								-122.888002,
+								49.001938
+							],
+							[
+								-122.888002,
+								49.1945832
+							],
+							[
+								-123.3520628,
+								49.1945832
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon May 28 04:16:37 +0000 2018",
 		"id": 1000953967713640400,
 		"id_str": "1000953967713640449",
