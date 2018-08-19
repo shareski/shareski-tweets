5659f0633fc29dfee136eba00838cc137e602dab
shareski <shareski@gmail.com>
2018-08-19 15:12:18 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 113603,
-	"created_at": "Sun Aug 19 20:12:17 +0000 2018"
+	"tweets": 113604,
+	"created_at": "Sun Aug 19 21:12:18 +0000 2018"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,542 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Sun Aug 19 20:49:18 +0000 2018",
+		"id": 1031281978371391500,
+		"id_str": "1031281978371391494",
+		"text": "RT @Denlesks: No idea who that cop is but if a NFL team doesn’t sign him this week they’re idiots  https://t.co/DqquziInup",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Denlesks",
+					"name": "Denlesks",
+					"id": 2703181339,
+					"id_str": "2703181339",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1030468065866043400,
+					"id_str": "1030468065866043392",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+					"url": "https://t.co/DqquziInup",
+					"display_url": "pic.twitter.com/DqquziInup",
+					"expanded_url": "https://twitter.com/Mens_Corner_/status/1030469349482668033/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 550,
+							"h": 304,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 304,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 550,
+							"h": 304,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1030469349482668000,
+					"source_status_id_str": "1030469349482668033",
+					"source_user_id": 212942638,
+					"source_user_id_str": "212942638"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1030468065866043400,
+					"id_str": "1030468065866043392",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+					"url": "https://t.co/DqquziInup",
+					"display_url": "pic.twitter.com/DqquziInup",
+					"expanded_url": "https://twitter.com/Mens_Corner_/status/1030469349482668033/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 550,
+							"h": 304,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 304,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 550,
+							"h": 304,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1030469349482668000,
+					"source_status_id_str": "1030469349482668033",
+					"source_user_id": 212942638,
+					"source_user_id_str": "212942638",
+					"video_info": {
+						"aspect_ratio": [
+							275,
+							152
+						],
+						"duration_millis": 13800,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1030468065866043392/pu/vid/324x180/5rIU_dLJ8NT3SCup.mp4?tag=5"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1030468065866043392/pu/pl/U3rG05mP1BcxyE1v.m3u8?tag=5"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1030468065866043392/pu/vid/550x304/_Dt4lsNEbD16lpY8.mp4?tag=5"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 212942638,
+							"id_str": "212942638",
+							"name": "Men's Corner",
+							"screen_name": "Mens_Corner_",
+							"location": "",
+							"description": "",
+							"url": "http://t.co/QrfCdxvIDX",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "http://t.co/QrfCdxvIDX",
+											"expanded_url": "http://www.mens-corner.net",
+											"display_url": "mens-corner.net",
+											"indices": [
+												0,
+												22
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 26379,
+							"friends_count": 4303,
+							"listed_count": 69,
+							"created_at": "Sun Nov 07 14:19:49 +0000 2010",
+							"favourites_count": 55,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": false,
+							"verified": false,
+							"statuses_count": 8149,
+							"lang": "en",
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "C0DEED",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/3077498527/5c4f3b7c0c41ea64ecd6ea6fb758a7f8_normal.jpeg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/3077498527/5c4f3b7c0c41ea64ecd6ea6fb758a7f8_normal.jpeg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/212942638/1357661154",
+							"profile_link_color": "1DA1F2",
+							"profile_sidebar_border_color": "C0DEED",
+							"profile_sidebar_fill_color": "DDEEF6",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": false,
+							"default_profile": true,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world.  Currently ranked #12. Building community for @DiscoveryED . FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								127,
+								150
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 37698,
+			"friends_count": 6911,
+			"listed_count": 1406,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 656,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 113942,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Aug 19 00:55:54 +0000 2018",
+			"id": 1030981649935216600,
+			"id_str": "1030981649935216640",
+			"text": "No idea who that cop is but if a NFL team doesn’t sign him this week they’re idiots  https://t.co/DqquziInup",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1030468065866043400,
+						"id_str": "1030468065866043392",
+						"indices": [
+							85,
+							108
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+						"url": "https://t.co/DqquziInup",
+						"display_url": "pic.twitter.com/DqquziInup",
+						"expanded_url": "https://twitter.com/Mens_Corner_/status/1030469349482668033/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 550,
+								"h": 304,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 550,
+								"h": 304,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 550,
+								"h": 304,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1030469349482668000,
+						"source_status_id_str": "1030469349482668033",
+						"source_user_id": 212942638,
+						"source_user_id_str": "212942638"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1030468065866043400,
+						"id_str": "1030468065866043392",
+						"indices": [
+							85,
+							108
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1030468065866043392/pu/img/2l3gbooX4If4QcXH.jpg",
+						"url": "https://t.co/DqquziInup",
+						"display_url": "pic.twitter.com/DqquziInup",
+						"expanded_url": "https://twitter.com/Mens_Corner_/status/1030469349482668033/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 550,
+								"h": 304,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 550,
+								"h": 304,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 550,
+								"h": 304,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1030469349482668000,
+						"source_status_id_str": "1030469349482668033",
+						"source_user_id": 212942638,
+						"source_user_id_str": "212942638",
+						"video_info": {
+							"aspect_ratio": [
+								275,
+								152
+							],
+							"duration_millis": 13800,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1030468065866043392/pu/vid/324x180/5rIU_dLJ8NT3SCup.mp4?tag=5"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1030468065866043392/pu/pl/U3rG05mP1BcxyE1v.m3u8?tag=5"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1030468065866043392/pu/vid/550x304/_Dt4lsNEbD16lpY8.mp4?tag=5"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false,
+							"source_user": {
+								"id": 212942638,
+								"id_str": "212942638",
+								"name": "Men's Corner",
+								"screen_name": "Mens_Corner_",
+								"location": "",
+								"description": "",
+								"url": "http://t.co/QrfCdxvIDX",
+								"entities": {
+									"url": {
+										"urls": [
+											{
+												"url": "http://t.co/QrfCdxvIDX",
+												"expanded_url": "http://www.mens-corner.net",
+												"display_url": "mens-corner.net",
+												"indices": [
+													0,
+													22
+												]
+											}
+										]
+									},
+									"description": {
+										"urls": []
+									}
+								},
+								"protected": false,
+								"followers_count": 26379,
+								"friends_count": 4303,
+								"listed_count": 69,
+								"created_at": "Sun Nov 07 14:19:49 +0000 2010",
+								"favourites_count": 55,
+								"utc_offset": null,
+								"time_zone": null,
+								"geo_enabled": false,
+								"verified": false,
+								"statuses_count": 8149,
+								"lang": "en",
+								"contributors_enabled": false,
+								"is_translator": false,
+								"is_translation_enabled": false,
+								"profile_background_color": "C0DEED",
+								"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+								"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+								"profile_background_tile": false,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/3077498527/5c4f3b7c0c41ea64ecd6ea6fb758a7f8_normal.jpeg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/3077498527/5c4f3b7c0c41ea64ecd6ea6fb758a7f8_normal.jpeg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/212942638/1357661154",
+								"profile_link_color": "1DA1F2",
+								"profile_sidebar_border_color": "C0DEED",
+								"profile_sidebar_fill_color": "DDEEF6",
+								"profile_text_color": "333333",
+								"profile_use_background_image": true,
+								"has_extended_profile": false,
+								"default_profile": true,
+								"default_profile_image": false,
+								"following": false,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "none"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2703181339,
+				"id_str": "2703181339",
+				"name": "Denlesks",
+				"screen_name": "Denlesks",
+				"location": "California",
+				"description": "Personal appearance is show fodder. War bumsmack. Favorite topics: Val Kilmer, Kirstie Alley, Global Warmi...",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5183,
+				"friends_count": 297,
+				"listed_count": 51,
+				"created_at": "Sun Aug 03 03:29:54 +0000 2014",
+				"favourites_count": 129430,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 6948,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/751343633186467841/J0CLvvGF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/751343633186467841/J0CLvvGF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2703181339/1424504458",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 14558,
+			"favorite_count": 35456,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 14558,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Aug 19 20:08:16 +0000 2018",
 		"id": 1031271649742598100,
 		"id_str": "1031271649742598146",
