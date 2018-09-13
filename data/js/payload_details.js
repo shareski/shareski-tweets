45daf0914976e705ae99825a1d8104d22faefc26
shareski <shareski@gmail.com>
2018-09-12 21:12:18 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 113829,
-	"created_at": "Thu Sep 13 01:12:17 +0000 2018"
+	"tweets": 113830,
+	"created_at": "Thu Sep 13 03:12:17 +0000 2018"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Thu Sep 13 02:34:16 +0000 2018",
+		"id": 1040066101302816800,
+		"id_str": "1040066101302816768",
+		"text": "@AkaBreeee https://t.co/QKjj1a7LO2",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AkaBreeee",
+					"name": "Brianna Lynn, NBCT",
+					"id": 566806253,
+					"id_str": "566806253",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1040066092171817000,
+					"id_str": "1040066092171816961",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dm8Oma7UcAE8vsz.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dm8Oma7UcAE8vsz.jpg",
+					"url": "https://t.co/QKjj1a7LO2",
+					"display_url": "pic.twitter.com/QKjj1a7LO2",
+					"expanded_url": "https://twitter.com/shareski/status/1040066101302816768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 252,
+							"h": 188,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 252,
+							"h": 188,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 252,
+							"h": 188,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1040066092171817000,
+					"id_str": "1040066092171816961",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dm8Oma7UcAE8vsz.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dm8Oma7UcAE8vsz.jpg",
+					"url": "https://t.co/QKjj1a7LO2",
+					"display_url": "pic.twitter.com/QKjj1a7LO2",
+					"expanded_url": "https://twitter.com/shareski/status/1040066101302816768/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 252,
+							"h": 188,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 252,
+							"h": 188,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 252,
+							"h": 188,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							63,
+							47
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Dm8Oma7UcAE8vsz.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1040058281283207200,
+		"in_reply_to_status_id_str": "1040058281283207170",
+		"in_reply_to_user_id": 566806253,
+		"in_reply_to_user_id_str": "566806253",
+		"in_reply_to_screen_name": "AkaBreeee",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world.  Currently ranked #12. Building community for @DiscoveryED . FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								127,
+								150
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 37861,
+			"friends_count": 6970,
+			"listed_count": 1408,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 660,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 114167,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Thu Sep 13 00:33:38 +0000 2018",
 		"id": 1040035743060643800,
 		"id_str": "1040035743060643840",
