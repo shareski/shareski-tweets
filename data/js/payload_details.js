1dd1fa632ce65c0acb08510f18c90f52f7c1d3de
shareski <shareski@gmail.com>
2018-11-09 22:12:18 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 124546,
-	"created_at": "Sat Nov 10 03:12:17 +0000 2018"
+	"tweets": 124547,
+	"created_at": "Sat Nov 10 04:12:17 +0000 2018"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,251 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sat Nov 10 03:13:16 +0000 2018",
+		"id": 1061094410371260400,
+		"id_str": "1061094410371260416",
+		"full_text": "@andrewmead73 @Wpg_BlueBombers @sskroughriders @bluebomber6 @MsWelbergen @Morhart I have these. https://t.co/9B4LM5rEJR",
+		"truncated": false,
+		"display_text_range": [
+			82,
+			95
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "andrewmead73",
+					"name": "Andrew Mead",
+					"id": 248957048,
+					"id_str": "248957048",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "Wpg_BlueBombers",
+					"name": "WPG Blue Bombers",
+					"id": 72339761,
+					"id_str": "72339761",
+					"indices": [
+						14,
+						30
+					]
+				},
+				{
+					"screen_name": "sskroughriders",
+					"name": "Saskatchewan Roughriders",
+					"id": 64826919,
+					"id_str": "64826919",
+					"indices": [
+						31,
+						46
+					]
+				},
+				{
+					"screen_name": "bluebomber6",
+					"name": "Jeff Hoeppner",
+					"id": 301855786,
+					"id_str": "301855786",
+					"indices": [
+						47,
+						59
+					]
+				},
+				{
+					"screen_name": "MsWelbergen",
+					"name": "Sandy Welbergen",
+					"id": 119784868,
+					"id_str": "119784868",
+					"indices": [
+						60,
+						72
+					]
+				},
+				{
+					"screen_name": "Morhart",
+					"name": "Alexander Morhart",
+					"id": 77297798,
+					"id_str": "77297798",
+					"indices": [
+						73,
+						81
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1061094405434527700,
+					"id_str": "1061094405434527746",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/DrnDvWUUUAIxQ0p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrnDvWUUUAIxQ0p.jpg",
+					"url": "https://t.co/9B4LM5rEJR",
+					"display_url": "pic.twitter.com/9B4LM5rEJR",
+					"expanded_url": "https://twitter.com/shareski/status/1061094410371260416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1061094405434527700,
+					"id_str": "1061094405434527746",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/DrnDvWUUUAIxQ0p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrnDvWUUUAIxQ0p.jpg",
+					"url": "https://t.co/9B4LM5rEJR",
+					"display_url": "pic.twitter.com/9B4LM5rEJR",
+					"expanded_url": "https://twitter.com/shareski/status/1061094410371260416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1061093626493784000,
+		"in_reply_to_status_id_str": "1061093626493784065",
+		"in_reply_to_user_id": 248957048,
+		"in_reply_to_user_id_str": "248957048",
+		"in_reply_to_screen_name": "andrewmead73",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world.  Currently ranked #12. Building community for @DiscoveryED . FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								127,
+								150
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 38259,
+			"friends_count": 7109,
+			"listed_count": 1414,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 664,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 114579,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/928803228304551936/MtOJ7saz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@andrewmead73 @Wpg_BlueBombers @sskroughriders @bluebomber6 @MsWelbergen @Morhart I have these. https://t.co/9B4LM5rEJR"
+	},
+	{
 		"created_at": "Sat Nov 10 03:12:15 +0000 2018",
 		"id": 1061094156339216400,
 		"id_str": "1061094156339216384",
