709c9bd5f1d63fc875942a6d38f2b390c28369ad
shareski <shareski@gmail.com>
2018-12-07 22:12:17 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 131808,
-	"created_at": "Sat Dec 08 02:12:17 +0000 2018"
+	"tweets": 131809,
+	"created_at": "Sat Dec 08 04:12:16 +0000 2018"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sat Dec 08 03:50:36 +0000 2018",
+		"id": 1071250664800612400,
+		"id_str": "1071250664800612353",
+		"full_text": "@SMooreBC @technolandy @globalreadaloud Things like this make it less valuable. I’ve not referenced or encouraged educators to use twitter for at least 5 years. I’m not sure I’d encourage students to use it either. My original comment was questioning what we should say to students.",
+		"truncated": false,
+		"display_text_range": [
+			40,
+			282
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SMooreBC",
+					"name": "Sarah",
+					"id": 272181366,
+					"id_str": "272181366",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "technolandy",
+					"name": "Ian Landy",
+					"id": 250734775,
+					"id_str": "250734775",
+					"indices": [
+						10,
+						22
+					]
+				},
+				{
+					"screen_name": "globalreadaloud",
+					"name": "Pernille Ripp",
+					"id": 782393737464774700,
+					"id_str": "782393737464774656",
+					"indices": [
+						23,
+						39
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1071244247607570400,
+		"in_reply_to_status_id_str": "1071244247607570433",
+		"in_reply_to_user_id": 272181366,
+		"in_reply_to_user_id_str": "272181366",
+		"in_reply_to_screen_name": "SMooreBC",
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world.  Currently ranked #12. Building community for @DiscoveryED . FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								127,
+								150
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 38325,
+			"friends_count": 7140,
+			"listed_count": 1413,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 666,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 114770,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1066718916695621632/cd6jHbKz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1066718916695621632/cd6jHbKz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SMooreBC @technolandy @globalreadaloud Things like this make it less valuable. I’ve not referenced or encouraged educators to use twitter for at least 5 years. I’m not sure I’d encourage students to use it either. My original comment was questioning what we should say to students."
+	},
+	{
 		"created_at": "Sat Dec 08 01:35:59 +0000 2018",
 		"id": 1071216790905204700,
 		"id_str": "1071216790905204737",
