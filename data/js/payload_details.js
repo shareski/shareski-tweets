16afbd17fad419b4208f6b562e5e1c52f31dc27b
shareski <shareski@gmail.com>
2019-02-16 19:22:20 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 132838,
-	"created_at": "Sat Feb 16 01:22:19 +0000 2019"
+	"tweets": 132839,
+	"created_at": "Sun Feb 17 01:22:19 +0000 2019"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,297 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sat Feb 16 21:31:56 +0000 2019",
+		"id": 1096884909212422100,
+		"id_str": "1096884909212422146",
+		"full_text": "These women are Canadian royalty. https://t.co/ZuLVlh6P1i",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZuLVlh6P1i",
+					"expanded_url": "https://twitter.com/devin_heroux/status/1096423161858535424",
+					"display_url": "twitter.com/devin_heroux/s…",
+					"indices": [
+						34,
+						57
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working every day to bring a little more joy into the world.  Currently ranked #12. Building community for @DiscoveryED . FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								127,
+								150
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 38548,
+			"friends_count": 7267,
+			"listed_count": 1409,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 684,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 115773,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1066718916695621632/cd6jHbKz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1066718916695621632/cd6jHbKz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1096423161858535400,
+		"quoted_status_id_str": "1096423161858535424",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ZuLVlh6P1i",
+			"expanded": "https://twitter.com/devin_heroux/status/1096423161858535424",
+			"display": "twitter.com/devin_heroux/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Feb 15 14:57:07 +0000 2019",
+			"id": 1096423161858535400,
+			"id_str": "1096423161858535424",
+			"full_text": "ON THIS DAY IN HISTORY \n\nFebruary 15, 1998. The first gold medal in women’s curling at the Olympics goes to the Canadian/Saskatchewan rink of Sandra Schmirler, Jan Betker, Joan McCusker and Marcia Gudereit. #cbccurl https://t.co/xUMB3tLGrr",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				215
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "cbccurl",
+						"indices": [
+							207,
+							215
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1096423157978841100,
+						"id_str": "1096423157978841088",
+						"indices": [
+							216,
+							239
+						],
+						"media_url": "http://pbs.twimg.com/media/DzdHDZbXQAA5gUa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DzdHDZbXQAA5gUa.jpg",
+						"url": "https://t.co/xUMB3tLGrr",
+						"display_url": "pic.twitter.com/xUMB3tLGrr",
+						"expanded_url": "https://twitter.com/Devin_Heroux/status/1096423161858535424/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 665,
+								"h": 449,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 665,
+								"h": 449,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 665,
+								"h": 449,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1096423157978841100,
+						"id_str": "1096423157978841088",
+						"indices": [
+							216,
+							239
+						],
+						"media_url": "http://pbs.twimg.com/media/DzdHDZbXQAA5gUa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DzdHDZbXQAA5gUa.jpg",
+						"url": "https://t.co/xUMB3tLGrr",
+						"display_url": "pic.twitter.com/xUMB3tLGrr",
+						"expanded_url": "https://twitter.com/Devin_Heroux/status/1096423161858535424/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 665,
+								"h": 449,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 665,
+								"h": 449,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 665,
+								"h": 449,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 26347394,
+				"id_str": "26347394",
+				"name": "Devin Heroux",
+				"screen_name": "Devin_Heroux",
+				"location": "Toronto, Ontario",
+				"description": "CBC News/Olympics Reporter. Based in Toronto. Born in Saskatoon. Cross-platform 🥌curling correspondent. Email: Devin.Heroux@cbc.ca",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 19035,
+				"friends_count": 2138,
+				"listed_count": 313,
+				"created_at": "Tue Mar 24 22:53:21 +0000 2009",
+				"favourites_count": 67958,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 83743,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1021149909339103235/-IdKPtm2_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021149909339103235/-IdKPtm2_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/26347394/1446262469",
+				"profile_link_color": "2FC2EF",
+				"profile_sidebar_border_color": "181A1E",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 35,
+			"favorite_count": 250,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "These women are Canadian royalty. https://t.co/ZuLVlh6P1i"
+	},
+	{
 		"created_at": "Fri Feb 15 20:23:42 +0000 2019",
 		"id": 1096505351283691500,
 		"id_str": "1096505351283691520",
