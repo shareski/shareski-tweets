91b33a994ba5da7dc78dccda18d2cbe9426ceeb6
shareski <shareski@gmail.com>
2019-11-08 19:22:19 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 134762,
-	"created_at": "Fri Nov 08 01:22:18 +0000 2019"
+	"tweets": 134763,
+	"created_at": "Sat Nov 09 01:22:18 +0000 2019"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 08 04:05:32 +0000 2019",
+		"id": 1192654361438150700,
+		"id_str": "1192654361438150656",
+		"full_text": "Free speech is such a tricky thing. https://t.co/jTetRSltNU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jTetRSltNU",
+					"expanded_url": "https://twitter.com/rickpardo/status/1192392368777039872",
+					"display_url": "twitter.com/rickpardo/stat…",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "I was told there would be snacks. Working every day to bring a little more joy into the world. Currently ranked #12. FYI: https://t.co/GsJVzEeMpH",
+			"url": "https://t.co/xNwidZoCqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xNwidZoCqU",
+							"expanded_url": "http://about.me/shareski",
+							"display_url": "about.me/shareski",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/GsJVzEeMpH",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								122,
+								145
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 39230,
+			"friends_count": 7714,
+			"listed_count": 1400,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 803,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 117923,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1066718916695621632/cd6jHbKz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1066718916695621632/cd6jHbKz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1192392368777040000,
+		"quoted_status_id_str": "1192392368777039872",
+		"quoted_status_permalink": {
+			"url": "https://t.co/jTetRSltNU",
+			"expanded": "https://twitter.com/rickpardo/status/1192392368777039872",
+			"display": "twitter.com/rickpardo/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 07 10:44:28 +0000 2019",
+			"id": 1192392368777040000,
+			"id_str": "1192392368777039872",
+			"full_text": "Tech and Liberty.  'No power-that-be likes disruption, or innovation they do not control, (we need)  more free thinkers, not fewer.' Why Facebook is making the right decision dealing with free speech \nhttps://t.co/Txa43qsmbP",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				224
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Txa43qsmbP",
+						"expanded_url": "https://stratechery.com/2019/tech-and-liberty/",
+						"display_url": "stratechery.com/2019/tech-and-…",
+						"indices": [
+							201,
+							224
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20853929,
+				"id_str": "20853929",
+				"name": "Rick Pardo",
+				"screen_name": "rickpardo",
+				"location": "Ontario, Canada",
+				"description": "@TVinnovates  @TVDSB Creating conditions: connecting minds https://t.co/Kadvxm1Gw4 It's all a mistake . until it works.",
+				"url": "https://t.co/evByyKHtBY",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/evByyKHtBY",
+								"expanded_url": "https://tvdsb.ca",
+								"display_url": "tvdsb.ca",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/Kadvxm1Gw4",
+								"expanded_url": "https://sites.google.com/gotvdsb.ca/school-in-a-school/about-us",
+								"display_url": "sites.google.com/gotvdsb.ca/sch…",
+								"indices": [
+									59,
+									82
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 952,
+				"friends_count": 593,
+				"listed_count": 79,
+				"created_at": "Sat Feb 14 15:57:44 +0000 2009",
+				"favourites_count": 873,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1787,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "181C1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1030559319664807936/MS1ukRTF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1030559319664807936/MS1ukRTF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/20853929/1495298429",
+				"profile_link_color": "CB9934",
+				"profile_sidebar_border_color": "454B52",
+				"profile_sidebar_fill_color": "0E1621",
+				"profile_text_color": "575E61",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Free speech is such a tricky thing. https://t.co/jTetRSltNU"
+	},
+	{
 		"created_at": "Thu Nov 07 03:01:32 +0000 2019",
 		"id": 1192275865582866400,
 		"id_str": "1192275865582866432",
