b5a130586e7ada9130d20892c40efae2dd31e277
shareski <shareski@gmail.com>
2020-09-17 19:22:20 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 136941,
-	"created_at": "Thu Sep 17 01:22:19 +0000 2020"
+	"tweets": 136942,
+	"created_at": "Fri Sep 18 01:22:19 +0000 2020"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,178 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 17 14:51:29 +0000 2020",
+		"id": 1306550833098756000,
+		"id_str": "1306606707477327873",
+		"full_text": "@shareski No cheering for me yet but I received this from a grade 4 student last night. ❤️❤️❤️ https://t.co/4LMTQMZCN8",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "shareski",
+					"name": "Dean Shareski",
+					"id": 739743,
+					"id_str": "739743",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1306550829034471400,
+					"id_str": "1306550829034471424",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EiHNDn2XgAAOB9R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiHNDn2XgAAOB9R.jpg",
+					"url": "https://t.co/4LMTQMZCN8",
+					"display_url": "pic.twitter.com/4LMTQMZCN8",
+					"expanded_url": "https://twitter.com/acraftymom/status/1306550833098756098/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 723,
+							"h": 831,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 592,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 723,
+							"h": 831,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1306550829034471400,
+					"id_str": "1306550829034471424",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EiHNDn2XgAAOB9R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiHNDn2XgAAOB9R.jpg",
+					"url": "https://t.co/4LMTQMZCN8",
+					"display_url": "pic.twitter.com/4LMTQMZCN8",
+					"expanded_url": "https://twitter.com/acraftymom/status/1306550833098756098/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 723,
+							"h": 831,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 592,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 723,
+							"h": 831,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1306449834447052800,
+		"in_reply_to_status_id_str": "1306449834447052800",
+		"in_reply_to_user_id": 739743,
+		"in_reply_to_user_id_str": "739743",
+		"in_reply_to_screen_name": "shareski",
+		"user": {
+			"id": 14964405,
+			"id_str": "14964405",
+			"name": "Shannon Bertels",
+			"screen_name": "acraftymom",
+			"location": "Ottawa",
+			"description": "Educator of grade 3/4 students. Fitness and sports enthusiast. Helping kids get moving, fit and active. Mother. Crafty at heart. Can be found outdoors.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3440,
+			"friends_count": 1936,
+			"listed_count": 224,
+			"created_at": "Sat May 31 16:40:27 +0000 2008",
+			"favourites_count": 10643,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 235,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1243650929854906369/TVZtsinB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1243650929854906369/TVZtsinB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14964405/1590596618",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@shareski No cheering for me yet but I received this from a grade 4 student last night. ❤️❤️❤️ https://t.co/4LMTQMZCN8"
+	},
+	{
 		"created_at": "Wed Sep 16 23:28:16 +0000 2020",
 		"id": 1306374371569672200,
 		"id_str": "1306374371569672192",
