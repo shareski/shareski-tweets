86742cddeb0c70df6eadc6f2854dd307cf74af97
shareski <shareski@gmail.com>
2021-07-05 19:22:18 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 138915,
-	"created_at": "Mon Jul 05 01:22:17 +0000 2021"
+	"tweets": 138916,
+	"created_at": "Tue Jul 06 01:22:17 +0000 2021"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Mon Jul 05 20:19:09 +0000 2021",
+		"id": 1412144037398823000,
+		"id_str": "1412144037398822920",
+		"full_text": "Indeed. https://t.co/w0ax8vxujp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			7
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/w0ax8vxujp",
+					"expanded_url": "https://twitter.com/amymiller/status/1213667015652560896",
+					"display_url": "twitter.com/amymiller/stat…",
+					"indices": [
+						8,
+						31
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Working daily to reduce my ignorance. Concurrently trying to identify and linger in delight. FYI: https://t.co/PHfvBH5u7p",
+			"url": "https://t.co/jBMEaODRso",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jBMEaODRso",
+							"expanded_url": "http://shareski.ca",
+							"display_url": "shareski.ca",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PHfvBH5u7p",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 38779,
+			"friends_count": 10042,
+			"listed_count": 1360,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 850,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 122473,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1368019232517066754/xDwCqD4y_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1368019232517066754/xDwCqD4y_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1213667015652561000,
+		"quoted_status_id_str": "1213667015652560896",
+		"quoted_status_permalink": {
+			"url": "https://t.co/w0ax8vxujp",
+			"expanded": "https://twitter.com/amymiller/status/1213667015652560896",
+			"display": "twitter.com/amymiller/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Jan 05 03:42:20 +0000 2020",
+			"id": 1213667015652561000,
+			"id_str": "1213667015652560896",
+			"full_text": "There aren’t many universal truths but one is that teenage boys like to jump up and tap stuff that’s high. Street signs, beams, archways.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				137
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 231910443,
+				"id_str": "231910443",
+				"name": "Amy Miller",
+				"screen_name": "amymiller",
+				"location": "From OAK // Thriving in LA",
+				"description": "A woman who does stand-up comedy. Can you imagine? \n\nAlbum on @KillRockStars // Host @whosyourgodcast\n\nhttps://t.co/SUQpW7j71H for live dates mama",
+				"url": "https://t.co/w3dNv6Fj35",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/w3dNv6Fj35",
+								"expanded_url": "https://m.youtube.com/watch?v=2wpA93fOcss",
+								"display_url": "m.youtube.com/watch?v=2wpA93…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/SUQpW7j71H",
+								"expanded_url": "http://amymillercomedy.com",
+								"display_url": "amymillercomedy.com",
+								"indices": [
+									103,
+									126
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 25120,
+				"friends_count": 3534,
+				"listed_count": 276,
+				"created_at": "Wed Dec 29 20:13:51 +0000 2010",
+				"favourites_count": 81764,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 38649,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "70A178",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1405433285665189893/lMlmQxYO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1405433285665189893/lMlmQxYO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/231910443/1623837483",
+				"profile_link_color": "AA2FDE",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2467,
+			"favorite_count": 31301,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Indeed. https://t.co/w0ax8vxujp"
+	},
+	{
 		"created_at": "Sun Jul 04 21:28:27 +0000 2021",
 		"id": 1411799089574973400,
 		"id_str": "1411799089574973442",
