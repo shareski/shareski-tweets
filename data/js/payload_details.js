df8f9d024f76b6f14d7858e33304a45b653fd426
shareski <shareski@gmail.com>
2022-09-29 19:22:18 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 141400,
-	"created_at": "Thu Sep 29 01:22:18 +0000 2022"
+	"tweets": 141401,
+	"created_at": "Fri Sep 30 01:22:17 +0000 2022"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 29 21:54:00 +0000 2022",
+		"id": 1575604833234522000,
+		"id_str": "1575604833234522112",
+		"full_text": "50 years ago yesterday I sat in Mrs. Duchenko's grade 2 classroom and she turned on the TV last period of the Summit Series so we could witness history. https://t.co/sWGMQe0u9A",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1575539544732356600,
+					"id_str": "1575539544732356608",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd1w3k1aUAAV1KO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd1w3k1aUAAV1KO.jpg",
+					"url": "https://t.co/sWGMQe0u9A",
+					"display_url": "pic.twitter.com/sWGMQe0u9A",
+					"expanded_url": "https://twitter.com/shareski/status/1575604833234522112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 300,
+							"h": 260,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 300,
+							"h": 260,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 300,
+							"h": 260,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1575539544732356600,
+					"id_str": "1575539544732356608",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd1w3k1aUAAV1KO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd1w3k1aUAAV1KO.jpg",
+					"url": "https://t.co/sWGMQe0u9A",
+					"display_url": "pic.twitter.com/sWGMQe0u9A",
+					"expanded_url": "https://twitter.com/shareski/status/1575604833234522112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 300,
+							"h": 260,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 300,
+							"h": 260,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 300,
+							"h": 260,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Trying to spread a little joy. Lots of silliness. Concurrently trying to identify and linger in delight. FYI: https://t.co/PHfvBH5u7p",
+			"url": "https://t.co/jBMEaODRso",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jBMEaODRso",
+							"expanded_url": "http://shareski.ca",
+							"display_url": "shareski.ca",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PHfvBH5u7p",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 38966,
+			"friends_count": 10681,
+			"listed_count": 1320,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 882,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 125320,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551800071959810051/NsUsM23z_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551800071959810051/NsUsM23z_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "50 years ago yesterday I sat in Mrs. Duchenko's grade 2 classroom and she turned on the TV last period of the Summit Series so we could witness history. https://t.co/sWGMQe0u9A"
+	},
+	{
 		"created_at": "Wed Sep 28 17:37:36 +0000 2022",
 		"id": 1575177922725695500,
 		"id_str": "1575177922725695488",
