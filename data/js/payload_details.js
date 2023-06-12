a9c4591bf6c635cf782410d56710f6dadab5aa89
shareski <shareski@gmail.com>
2023-06-11 19:22:18 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 142112,
-	"created_at": "Sun Jun 11 01:22:17 +0000 2023"
+	"tweets": 142113,
+	"created_at": "Mon Jun 12 01:22:17 +0000 2023"
 }
--- data/js/tweets/2023_06.js
+++ data/js/tweets/2023_06.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2023_06 = 
 [
 	{
+		"created_at": "Mon Jun 12 00:17:11 +0000 2023",
+		"id": 1668049771674607600,
+		"id_str": "1668049771674607617",
+		"full_text": "And @ahadwingolf trending ahead of @ntaylorgolf59 Wondering if all the attention @jessicahadwin gets is leading to this behaviour. https://t.co/NdSpd20JB1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ahadwingolf",
+					"name": "adam hadwin",
+					"id": 244768359,
+					"id_str": "244768359",
+					"indices": [
+						4,
+						16
+					]
+				},
+				{
+					"screen_name": "ntaylorgolf59",
+					"name": "Nick Taylor",
+					"id": 293840625,
+					"id_str": "293840625",
+					"indices": [
+						35,
+						49
+					]
+				},
+				{
+					"screen_name": "jessicahadwin",
+					"name": "Jessica Hadwin",
+					"id": 379997829,
+					"id_str": "379997829",
+					"indices": [
+						81,
+						95
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1668049766570426400,
+					"id_str": "1668049766570426369",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/FyYabMwagAE3tmg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FyYabMwagAE3tmg.jpg",
+					"url": "https://t.co/NdSpd20JB1",
+					"display_url": "pic.twitter.com/NdSpd20JB1",
+					"expanded_url": "https://twitter.com/shareski/status/1668049771674607617/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 737,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1905,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 418,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1668049766570426400,
+					"id_str": "1668049766570426369",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/FyYabMwagAE3tmg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FyYabMwagAE3tmg.jpg",
+					"url": "https://t.co/NdSpd20JB1",
+					"display_url": "pic.twitter.com/NdSpd20JB1",
+					"expanded_url": "https://twitter.com/shareski/status/1668049771674607617/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 737,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1905,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 418,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739743,
+			"id_str": "739743",
+			"name": "Dean Shareski",
+			"screen_name": "shareski",
+			"location": "Moose Jaw, SK",
+			"description": "Trying to spread a little joy. Lots of silliness. Concurrently trying to identify and linger in delight. Former blue check mark guy. FYI: https://t.co/PHfvBHnDlx",
+			"url": "https://t.co/jBMEaOW0Gw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jBMEaOW0Gw",
+							"expanded_url": "http://shareski.ca",
+							"display_url": "shareski.ca",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PHfvBHnDlx",
+							"expanded_url": "http://shareski.ca/y/followme",
+							"display_url": "shareski.ca/y/followme",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 37796,
+			"friends_count": 10576,
+			"listed_count": 1282,
+			"created_at": "Wed Jan 31 19:59:13 +0000 2007",
+			"favourites_count": 918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 126167,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666BFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1635046446264139776/IPWnWEHH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1635046446264139776/IPWnWEHH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739743/1478275492",
+			"profile_link_color": "384D3A",
+			"profile_sidebar_border_color": "CCC333",
+			"profile_sidebar_fill_color": "1DB2D7",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "And @ahadwingolf trending ahead of @ntaylorgolf59 Wondering if all the attention @jessicahadwin gets is leading to this behaviour. https://t.co/NdSpd20JB1"
+	},
+	{
 		"created_at": "Sun Jun 11 00:22:50 +0000 2023",
 		"id": 1667688806483468300,
 		"id_str": "1667688806483468288",
